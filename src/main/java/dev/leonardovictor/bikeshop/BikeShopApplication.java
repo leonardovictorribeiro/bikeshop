5475897784778c3b659ef9421bae9ccc5f20e962
Leonardo Victor Ribeiro <leonardovictor@outlook.com>
2023-02-11 21:09:18 -0300

    adiciona bean do OpenAPI

--- src/main/java/dev/leonardovictor/bikeshop/BikeShopApplication.java
+++ src/main/java/dev/leonardovictor/bikeshop/BikeShopApplication.java
@@ -1,7 +1,13 @@
 package dev.leonardovictor.bikeshop;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class BikeShopApplication {
@@ -10,4 +16,16 @@ public class BikeShopApplication {
 		SpringApplication.run(BikeShopApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${app.description}") String appDescription, 
+	@Value("${app.version}") String appVersion) {
+		return new OpenAPI()
+		.info(new Info()
+		.title("Oficina API")
+		.version(appVersion)
+		.description(appDescription)
+		.termsOfService("http://swagger.io/terms/")
+		.license(new License()
+		.name("Apache 2.0").url("http://springdoc.org/")));
+	}
 }
